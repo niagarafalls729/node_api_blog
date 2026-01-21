9b536f35600c2020dc09246cf4ddd03376a95d8e
jisukim92 <jisu.kim92@hancommds.com>
2026-01-21 13:30:26 +0900

    fix :서버 변경으로 인한 수정 #2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
     instances: 2, // 2~3개 권장 (CPU 코어 수에 맞춤)
     exec_mode: "cluster", // 클러스터 모드 (여러 프로세스 실행)
     watch: true, // 파일 변경 감지 후 자동 재시작
-    ignore_watch: ["node_modules", "log", "public/uploads", "*.log"], // 변경 감지 제외 경로
+    ignore_watch: ["node_modules", "log", "public/uploads", "public/crawled_images", "*.log"], // 변경 감지 제외 경로
     env: {
       NODE_ENV: "production"
     }
